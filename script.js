a481c614ba7a00a75bb1259f4a5756b21c4c9941
ibudnitski <igor.budnitski@gmail.com>
2023-12-11 15:45:00 +0200

    Commit

--- script.js
+++ script.js
@@ -32,30 +32,37 @@ let playList = {
 }
 
 function renderPlaylist(playListForRendering){
-    /* Creating Heading, main image, and information about playlist (track count and tracks duration)*/
+    renderPlaylistHeader(playListForRendering);
+    renderTrack(playListForRendering);
+}
+
+renderPlaylist(playList);
+
+function renderPlaylistHeader(playListForRendering){
+     /* Creating Heading, main image, and information about playlist (track count and tracks duration)*/
 const playListTitle = document.createElement('h1');
-playListTitle.append(playList.title);
-document.body.append(playListTitle);
+playListTitle.append(playListForRendering.title);
+document.body.append(playListForRendering);
 
 
 const playListImage = document.createElement('img');
-playListImage.src = playList.coverImg;
+playListImage.src = playListForRendering.coverImg;
 document.body.append(playListImage);
 
 
 const trackCount = document.createElement('span');
 trackCount.setAttribute("id", "span1");
-trackCount.append("Track count: " + playList.info.totalTrackCount);
+trackCount.append("Track count: " + playListForRendering.info.totalTrackCount);
 document.body.append(trackCount);
 
 
 const playListDuration = document.createElement('span');
 playListDuration.setAttribute("id", "span2");
-playListDuration.append("Track duration: " + Math.floor(playList.info.totalTracksDurationInSeconds / 60) + " m " + playList.info.totalTracksDurationInSeconds % 60 + " s");
+playListDuration.append("Track duration: " + Math.floor(playListForRendering.info.totalTracksDurationInSeconds / 60) + " m " + playListForRendering.info.totalTracksDurationInSeconds % 60 + " s");
 document.body.append(playListDuration);
-
-
-/* Adding music */
+  } 
+function renderTrack(playListForRendering){
+    /* Adding music */
 
 // creating img elements
 const img1 = document.createElement('img');
@@ -63,9 +70,9 @@ const img2 = document.createElement('img');
 const img3 = document.createElement('img');
 
 // adding images
-img1.src = playList.tracks[0].artistImage;
-img2.src = playList.tracks[1].artistImage;
-img3.src = playList.tracks[2].artistImage;
+img1.src = playListForRendering.tracks[0].artistImage;
+img2.src = playListForRendering.tracks[1].artistImage;
+img3.src = playListForRendering.tracks[2].artistImage;
 
 // crating audio elements
 const audio1 = document.createElement('audio');
@@ -73,11 +80,11 @@ const audio2 = document.createElement('audio');
 const audio3 = document.createElement('audio');
 
 //adding src + controls
-audio1.src = playList.tracks[0].fileUrl;
+audio1.src = playListForRendering.tracks[0].fileUrl;
 audio1.controls = 'controls';
-audio2.src = playList.tracks[1].fileUrl;
+audio2.src = playListForRendering.tracks[1].fileUrl;
 audio2.controls = 'controls';
-audio3.src = playList.tracks[2].fileUrl;
+audio3.src = playListForRendering.tracks[2].fileUrl;
 audio3.controls = 'controls';
 
 // creating UL
@@ -89,9 +96,9 @@ const listItem2 = document.createElement('li');
 const listItem3 = document.createElement('li');
 
 // Adding information to LI image + artist name + title + audio
-listItem1.append(img1, playList.tracks[0].artistName + " - " + playList.tracks[0].title, audio1);
-listItem2.append(img2, playList.tracks[1].artistName + " - " + playList.tracks[1].title, audio2);
-listItem3.append(img3, playList.tracks[2].artistName + " - " + playList.tracks[2].title, audio3);
+listItem1.append(img1, playListForRendering.tracks[0].artistName + " - " + playListForRendering.tracks[0].title, audio1);
+listItem2.append(img2, playListForRendering.tracks[1].artistName + " - " + playListForRendering.tracks[1].title, audio2);
+listItem3.append(img3, playListForRendering.tracks[2].artistName + " - " + playListForRendering.tracks[2].title, audio3);
 
 // adding Li to UL
 mainLi.append(listItem1);
@@ -100,10 +107,7 @@ mainLi.append(listItem3);
 
 //adding UL to body
 document.body.append(mainLi);
-
-}
-
-renderPlaylist(playList);
+  } 
 
 
 
